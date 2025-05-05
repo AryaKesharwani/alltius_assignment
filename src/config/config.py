da4c7992f334876abf549b795c2cdc95c74bec33
Arya kesarwani <aryakesharwani1234@gmail.com>
2025-05-06 02:59:39 +0530

    added config

---
+++ src/config/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "gcp-starter")
